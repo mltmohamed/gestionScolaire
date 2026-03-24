42713ec8db34482004907e09a99b91c2854a0856
mohTraCode <traoremohamed7212@gmail.com>
2026-03-24 12:39:23 +0000

    modal du professeur terminer

--- src/renderer/src/pages/Teachers.jsx
+++ src/renderer/src/pages/Teachers.jsx
@@ -248,7 +248,7 @@ export default function Teachers() {
 
       {/* Dialog d'ajout/modification */}
       <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-        <DialogContent className="max-w-2xl">
+        <DialogContent className="max-w-2xl max-h-[90vh] overflow-y-auto">
           <form onSubmit={handleSubmit}>
             <DialogHeader>
               <DialogTitle>
